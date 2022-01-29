7a65cdf4b84118d1292dc9fe2b4f2c4b84adac93
Mariana Saraiva <marianascm8@gmail.com>
2022-01-29 20:43:04 -0300

    Requisitos 70 ao 74

--- src/pages/ExploreFoods.js
+++ src/pages/ExploreFoods.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function ExploreFoods() {
+  const history = useHistory();
   return (
     <div>
       <Header title="Explore Foods" />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explore/foods/ingredients') }
+      >
+        By Ingredient
+      </button>
+      <button
+        type="button"
+        data-testid="explore-by-nationality"
+        onClick={ () => history.push('/explore/foods/nationalities') }
+      >
+        By Nationality
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ () => history.push('/foods/52771') }
+      >
+        Surprise me!
+      </button>
       <Footer />
     </div>
   );
